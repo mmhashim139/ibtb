bf81eab69cf3f69ed4f5c5d025b3e2f75673eae6
Mohamed Hashim <mmhashim139@gmail.com>
2020-08-03 22:59:30 +0000

    Retrieve new API data in new cards

--- script.js
+++ script.js
@@ -11,7 +11,10 @@ const placeCountry = document.getElementById('place-country');
 const placeRegion = document.getElementById('place-region'); 
 const placeLocality = document.getElementById('place-locality'); 
 const placeTags = document.getElementById('place-tags'); 
+const cardDeck = document.getElementById('card-deck')
 
+
+let data = [];
 
 // Fetch API 
 const apiUrl = `https://failteireland.azure-api.net/opendata-api/v1/attractions`;
@@ -25,21 +28,61 @@ fetch(apiUrl, {
   })
       .then(res => res.json())
       .then(data => {
-       for ( i = 0 ; i<=49 ; i++) {
-        placeName.innerHTML = data.results[i].name ,
-        placeImage.innerHTML = `<img class="card-img-top place-img" id="place-image" src="${data.results[i].image.url}" alt="Card image cap">`,
-        placeUrl.innerHTML = `<a href="${data.results[i].url}" target="_blank"> Website </a>`,
-        placeTel.innerHTML =  `<a href="tel:${data.results[i].telephone}">${data.results[0].telephone}</a>`,
-        placeCountry.innerHTML = data.results[i].address.addressCountry ,
-        placeRegion.innerHTML = data.results[i].address.addressRegion ,
-        placeLocality.innerHTML = data.results[i].address.addressLocality ,
-        placeTags.innerHTML = data.results[i].tags ,
-        placeMapLocation.innerHTML = `<a href="http://www.google.com/maps/place/${data.results[i].geo.latitude},${data.results[i].geo.longitude}" target="_blank" > Location</a> `
-        }    
+          for (i = 0 ; i<=3000 ; i++) {
+          const place = data.results[i];
+          const newPlace = {
+              name : `${place.name}`,
+              imageUrl : `${place.image.url}`,
+              websiteUrl : `${place.url}`,
+              tel : `${place.telephone}`,
+              country :`${place.address.addressCountry}`,
+              region : `${place.address.addressRegion}`,
+              locality : `${place.address.addressLocality}`,
+              tags : `${place.tags}`,
+              mapUrl : `http://www.google.com/maps/place/${place.geo.latitude},${place.geo.longitude}`
+          }
+        addNewPlace(newPlace);
+          }
       })
+
     .catch(function(error) {
          console.log(error);
     });
+
+     function addNewPlace(obj) {
+     data.push(obj);
+     updateDOM();
+    }
+
+    function updateDOM(providedData = data) {
+  // Clear main div
+        
+    providedData.forEach(item => {
+    const element = document.createElement('div');
+    element.classList.add('container');
+    element.innerHTML = `
+                <div class="card place-card" id="place-card">
+                <div id="place-image"><img class="card-img-top" src="${item.imageUrl}" alt="Card image cap"></div>
+				<div class="card-body">
+					<div class="card-title" id="place-name">${item.name}</div>
+                    <div class="card-text" id="place-country">${item.country}</div>
+                    <div class="card-text" id="place-region">${item.region}</div>
+                    <div class="card-text" id="place-locality">${item.locality}</div>
+                    <div class="card-text" id="place-tags">${item.tags} </div>
+					<div class="card-text" id="place-url"><small class="text-muted"><a href="${item.websiteUrl}" target="_blank"> Website </a></small></div>
+                    <div class="card-text" id="place-tel"><small class="text-muted"><a href="tel:${item.tel}">${item.tel}</a></small></div>
+                    <div class="card-text" id="place-map-location"><a href="${item.mapUrl}" target="_blank" > Location</a></div> 
+				</div>
+			</div>
+
+      `
+        cardDeck.appendChild(element);
+      });
+      
+}
+
+    
+
 
     
 
