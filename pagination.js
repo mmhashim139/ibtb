d86bee3d31c7532d56bb26d7fc30ad60a0a04075
Mohamed Hashim <mmhashim139@gmail.com>
2020-08-15 00:41:06 +0000

    add button to show more reults

---
+++ pagination.js
@@ -0,0 +1,20 @@
+//define variables
+
+const more = document.getElementById('show-more');
+const pagesUrl = `https://failteireland.azure-api.net/opendata-api/v1/activities?subscription-key=&search=*&$skip=`;
+let pageNum = 50;
+
+// Add Event lisitener to show all places when all places btn clicked
+
+more.addEventListener("click", () => { 
+    
+    cardDeck.innerHTML = "";
+    // call fetchData Function and update DOM 
+    fetchData(`${pagesUrl}${pageNum}`, updateDOM);
+     pageNum = pageNum +50 ;
+    });
+
+
+
+
+
